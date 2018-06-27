9a987e4e0c254028a790ed4de82ecbc5e6986b00
Olav Frengstad <olav@tiny-mesh.com>
2018-06-27 21:14:48 +0200

    tmp - fix disabling probing

--- os/net/routing/rpl-classic/rpl.c
+++ os/net/routing/rpl-classic/rpl.c
@@ -266,11 +266,13 @@ rpl_link_callback(const linkaddr_t *addr, int status, int numtx)
     if(instance->used == 1 ) {
       parent = rpl_find_parent_any_dag(instance, &ipaddr);
       if(parent != NULL) {
+#if RPL_WITH_PROBING
         /* If this is the neighbor we were probing urgently, mark urgent
         probing as done */
         if(instance->urgent_probing_target == parent) {
           instance->urgent_probing_target = NULL;
         }
+#endif /* RPL_WITH_PROBING */
         /* Trigger DAG rank recalculation. */
         PRINTF("RPL: rpl_link_callback triggering update\n");
         parent->flags |= RPL_PARENT_FLAG_UPDATED;
